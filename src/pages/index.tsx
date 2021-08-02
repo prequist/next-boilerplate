468992855a83810f405b9f29d47037e8480aac7d
Alistair Smith <alistairsmith01@gmail.com>
2021-08-03 00:28:42 +0100

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import type {HandlerResponse} from './api/hello';
+import type {APIResponse} from 'nextkit';
+
+import {useEffect, useState} from 'react';
+
+export default function Home() {
+	return <div>hi</div>;
+}
