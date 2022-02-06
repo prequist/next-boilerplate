14c30cef90f98fbed9b577f0082ccd1218ae6e4e
Alistair Smith <hi@alistair.sh>
2022-02-06 20:20:33 +0000

    fix: “use nextkit/client

--- src/client/fetcher.ts
+++ src/client/fetcher.ts
@@ -1,11 +1,12 @@
-import {APIResponse, NextkitException} from 'nextkit';
+import {APIResponse} from 'nextkit';
+import {NextkitClientException} from 'nextkit/client';
 
 export async function fetcher<T>(url: string) {
 	const request = await fetch(url);
 	const body = (await request.json()) as APIResponse<T>;
 
 	if (!body.success) {
-		throw new NextkitException(request.status, body.message);
+		throw new NextkitClientException(request.status, body.message);
 	}
 
 	return body.data;
